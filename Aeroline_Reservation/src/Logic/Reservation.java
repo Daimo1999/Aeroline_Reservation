0e6fa64b1d54385a0dcbe713e7ea3c0b9304c8dc
Edwin <Edwin@DESKTOP-L8CTB4G>
2019-02-18 20:13:07 -0600

    metodos

--- Aeroline_Reservation/src/Logic/Reservation.java
+++ Aeroline_Reservation/src/Logic/Reservation.java
@@ -15,6 +15,7 @@ import models.Passenger;
 public class Reservation {
     private Passenger[][] economicClass;
     private Passenger[][] ejecutiveClass;
+    private Passenger[] passengerList;
 
 
     public Reservation(int rows, int columns) {
@@ -23,56 +24,54 @@ public class Reservation {
 
              economicClass = new Passenger[rows][columns*2];
         }else{
-            JOptionPane.showMessageDialog(null, "la cantidad de filas debe de ser de entre 2 y 4", "Error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "la cantidad de columnas del avion debe de ser de entre 2 y 4", "Error", JOptionPane.ERROR_MESSAGE);
         }
        
     }
-    public void addReservation(int decision,int row, int column, Passenger passenger) {
-        if (decision == 1) {
-            for (int i = 0; i < economicClass.length; i++) {
-                for (int j = 0; j < economicClass[i].length; j++) {
-                    economicClass[row][column] = passenger; 
-                }
+    public void addReservation() {
+        int counter = 0;
+        int question = 0;
+        String code = "";
+        do {
+       
+            int decision = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el tipo de reservacion \n1 = Clase economica \n2 = Clase ejecutiva"));
+            int row = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la fila"));
+            int column = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la columna"));
+            if (decision == 1 && economicClass[row][column] == null) {
+                Passenger passenger = new Passenger();
+                economicClass[row][column] = passenger;
+                economicClass[row][column].setName(JOptionPane.showInputDialog("Ingrese el nombre del pasajero"));
+                economicClass[row][column].setLastName(JOptionPane.showInputDialog("Ingrese el apellido del pasajero"));
+                economicClass[row][column].setPassportNumber(Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de pasaporte del pasajero")));
+                question = JOptionPane.showConfirmDialog(null, "Desea agregar otro pasajero");
+                System.out.println(question);
+
+            }else if(decision == 2 && ejecutiveClass[row][column] == null) {
+                Passenger passenger = new Passenger();            
+                ejecutiveClass[row][column] = passenger;
+                ejecutiveClass[row][column].setName(JOptionPane.showInputDialog("Ingrese el nombre del pasajero"));
+                ejecutiveClass[row][column].setLastName(JOptionPane.showInputDialog("Ingrese el apellido del pasajero"));
+                ejecutiveClass[row][column].setPassportNumber(Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de pasaporte del pasajero")));
+                 question = JOptionPane.showConfirmDialog(null, "Desea agregar otro pasajero");    
+        
+            }else{
+                System.out.println("Este lugar ya posee una reservación");
             }
-        }else if(decision == 2) {
-            for (int i = 0; i < ejecutiveClass.length; i++) {
-                for (int j = 0; j < ejecutiveClass.length; j++) {
-                    ejecutiveClass[row][column] = passenger; 
-                }
-            } 
-        }
+             counter += 1;
+        } while (counter != 5 && question != 1);
     }
-    public String stringMatrix() {
-        String stringMatrix = "";
-        int[] numbArray = new int[40];
-        for (int i = 0; i < numbArray.length; i++) {
-           numbArray[i] = i+1;
-        }
-        String[] code = new String[]{"A ","B ","C ","D ","E ", "F ", "G ", "H "};
-         for (int i = 0; i < ejecutiveClass.length; i++) {
-            for (int j = 0; j < ejecutiveClass[i].length; j++) {
-                stringMatrix += numbArray[i] + "";
-                stringMatrix +=  "" + code[j];
-                stringMatrix += " "; 
-               
-            }
-            
-            stringMatrix += "\n";
-        }
+    public String print() {
+        String print = "";
         for (int i = 0; i < economicClass.length; i++) {
             for (int j = 0; j < economicClass[i].length; j++) {
-               stringMatrix += numbArray[i] + "";
-               stringMatrix += "" + code[j];
-               stringMatrix += " ";
-               
+               print += economicClass[i][j];
+                
             }
             
-            stringMatrix += "\n";
         }
-        
-        return stringMatrix;
-    }
-    
+        return print;
+    } 
+  
     public void ModifyReservation() {
         
     
--- Aeroline_Reservation/src/NewMain.java
+++ Aeroline_Reservation/src/NewMain.java
@@ -1,6 +1,8 @@
 
 import Logic.Reservation;
 import java.util.Arrays;
+import javax.swing.JOptionPane;
+import models.Passenger;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -18,8 +20,11 @@ public class NewMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Passenger passenger1 = new Passenger(702700150, "Edwin", "Molina");
+         Passenger passenger2 = new Passenger(702700150, "Ed", "Molina");
         Reservation reservation = new Reservation(4, 3);
-        System.out.println(reservation.stringMatrix());
+       reservation.addReservation();
+        System.out.println(reservation.print());
     }
     
 }
